636e9d0989b8e6a5851577099f25adef3664ab20
czM1K3 <misamadera1@gmail.com>
2021-12-22 08:43:20 +0100

    Codeventure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Codeventure</a>
         </h1>
 
         <p className={styles.description}>
